af96b3a9c48ef67881e4bd04fc9bcdc7e30d7f4a
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2024-12-20 11:08:06 +0530

    Update todoitem.jsx

--- src/components/todoitem.jsx
+++ src/components/todoitem.jsx
@@ -7,7 +7,7 @@ return  <div className="container">
   <div className="col-2"><button type="button" className="btn btn-danger sbtn">delete</button></div>
 </div>
 <div className="row">
-  <div className="col-6">buy m</div>
+  <div className="col-6">buy mobile</div>
   <div className="col-4">12/02/2024</div>
   <div className="col-2"><button type="button" className="btn btn-danger sbtn">delete</button></div>
 </div>
