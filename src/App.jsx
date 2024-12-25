d473eb72f6f44224e414c39e656ab9279a615ce5
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2024-12-25 12:49:59 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import AppName from "./components/appname"
 import Todoitme1 from "./components/todoitem"
 import "./App.css";
 function App() {
+  let tasks=[
+    {task:"buy milk",date:"12/12/2024"},
+    {task:"buy mobile",date:"12/02/2024"},
+    {task:"vegitables",date:"23/12/2024"}
+  ];
   return <center className="todo-container">
    <AppName/>
    <Addtodo/>
-   <Todoitme1/>
+   <Todoitme1 todo={tasks} />
   </center>
 }
 
--- src/components/todoitem.jsx
+++ src/components/todoitem.jsx
@@ -1,17 +1,10 @@
-function Todoitme1(){
+function Todoitme1({todo}){
 return  <div className="container">
-    
-<div className="row">
-  <div className="col-6">buy milk</div>
-  <div className="col-4">12/12/2024</div>
+   {todo.map((item,index)=>(<div key={index} className="row">
+  <div className="col-6">{item.task}</div>
+  <div className="col-4">{item.date}</div>
   <div className="col-2"><button type="button" className="btn btn-danger sbtn">delete</button></div>
-</div>
-<div className="row">
-  <div className="col-6">buy mobile</div>
-  <div className="col-4">12/02/2024</div>
-  <div className="col-2"><button type="button" className="btn btn-danger sbtn">delete</button></div>
-</div>
-
+</div>))} 
 </div>
 }
 export default Todoitme1;
